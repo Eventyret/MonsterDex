c351388a761e715b0df04ad1916613c63d4ce51d
Eventyret <simen.dehlin@gmail.com>
2019-09-04 17:01:03 +0100

    Removed prop not needed

--- src/App.js
+++ src/App.js
@@ -20,7 +20,7 @@ class App extends Component {
   render() {
     return (
       <div className='App'>
-        <CardList name='Simen'>
+        <CardList>
         {this.state.monsters.map(monster => (
           <h1 key={monster.id}>{monster.name}</h1>
         ))}
