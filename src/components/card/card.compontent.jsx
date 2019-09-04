cfae4ebb584bebfd531f7edee55f4ac697b76fb2
Eventyret <simen.dehlin@gmail.com>
2019-09-04 17:13:16 +0100

    Created monster images

--- src/components/card/card.compontent.jsx
+++ src/components/card/card.compontent.jsx
@@ -2,6 +2,7 @@ import React from 'react';
 import './card.styles.css';
 export const Card = props => (
   <div class='card-container'>
+  <img alt="monster" src={`https://robohash.org/${props.monster.id}?set=set2`} />
     <h1> {props.monster.name}</h1>
   </div>
 );
