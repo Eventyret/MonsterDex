ea1a1603148946b43349b6656dd0ac194144d580
Eventyret <simen.dehlin@gmail.com>
2019-09-04 17:16:33 +0100

    Email added images working

--- src/components/card/card.compontent.jsx
+++ src/components/card/card.compontent.jsx
@@ -2,7 +2,8 @@ import React from 'react';
 import './card.styles.css';
 export const Card = props => (
   <div class='card-container'>
-  <img alt="monster" src={`https://robohash.org/${props.monster.id}?set=set2`} />
-    <h1> {props.monster.name}</h1>
+    <img alt='monster' src={`https://robohash.org/${props.monster.id}?set=set2&size=180x180`} />
+    <h2> {props.monster.name}</h2>
+    <p>{props.monster.email}</p>
   </div>
 );
