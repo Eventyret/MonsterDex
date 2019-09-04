8025ab21ae335a33b54970619d7254fd96dc2a44
Eventyret <simen.dehlin@gmail.com>
2019-09-04 15:15:31 +0100

    Added to monsters array with set state

--- src/App.js
+++ src/App.js
@@ -6,27 +6,14 @@ class App extends Component {
     super();
 
     this.state = {
-      monsters: [
-        {
-          name: 'Frankenstain',
-          id: 1
-        },
-        {
-          name: 'Dracula',
-          id: 2
-        },
-        {
-          name: 'Zombie',
-          id: 3
-        }
-      ]
+      monsters: []
     };
   }
-  
+
   componentDidMount() {
     fetch('https://jsonplaceholder.typicode.com/users')
       .then(res => res.json())
-      .then(data => console.log(data));
+      .then(data => this.setState({ monsters: data }));
   }
 
   render() {
