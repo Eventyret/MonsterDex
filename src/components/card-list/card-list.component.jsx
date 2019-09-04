699b6177c802e23efd6565fe9e7323f2834e54ab
Eventyret <simen.dehlin@gmail.com>
2019-09-04 17:05:41 +0100

    Moved monsters to card-list

--- src/components/card-list/card-list.component.jsx
+++ src/components/card-list/card-list.component.jsx
@@ -1,6 +1,12 @@
 import React from 'react';
-import './card-list.styles.css'
+import './card-list.styles.css';
+
 export const CardList = props => {
-  console.log(props);
-  return <div className="card-list">{props.children}</div>;
+  return (
+    <div className='card-list'>
+      {props.monsters.map(monster => (
+        <h1 key={monster.id}>{monster.name}</h1>
+      ))}
+    </div>
+  );
 };
