97a4eb1aa35184bf1a1910907b67b2d3d1439997
Eventyret <simen.dehlin@gmail.com>
2019-09-04 17:10:18 +0100

    Passing monster to individual card

--- src/components/card/card.compontent.jsx
+++ src/components/card/card.compontent.jsx
@@ -1,7 +1,7 @@
 import React from 'react';
 
-export const Card = () => {
-	<div>
-	<h1>{monster.name}</h1>
-	</div>
-}
+export const Card = props => (
+  <div>
+    <h1> {props.monster.name}</h1>
+  </div>
+);
