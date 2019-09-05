8e7f1ff53bce77d50a6a0e1b9d29e11e5872a0f4
Eventyret <simen.dehlin@gmail.com>
2019-09-05 18:46:30 +0100

    Made Searchbox component

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import React, { Component } from 'react';
 import { CardList } from './components/card-list/card-list.component';
+import { SearchBox } from './components/search-box/search-box.components';
 import './App.css';
 
 class App extends Component {
@@ -23,11 +24,7 @@ class App extends Component {
     const filteredMonsters = monsters.filter(monster => monster.name.toLowerCase().includes(searchField.toLowerCase()));
     return (
       <div className='App'>
-        <input
-          type='search'
-          placeholder='Search For a Monster'
-          onChange={e => this.setState({ searchField: e.target.value })}
-        />
+        <SearchBox placeholder='Search Monsters' handleChange={e => this.setState({ searchField: e.target.value })} />
         <CardList monsters={filteredMonsters} />
       </div>
     );
