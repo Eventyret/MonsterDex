b54a2da17bd02c63ab840fffe72546e3c70f79ab
Eventyret <simen.dehlin@gmail.com>
2019-09-05 17:49:26 +0100

    Added input for search

--- src/App.js
+++ src/App.js
@@ -7,7 +7,8 @@ class App extends Component {
     super();
 
     this.state = {
-      monsters: []
+      monsters: [],
+      searchField: ''
     };
   }
 
@@ -20,8 +21,12 @@ class App extends Component {
   render() {
     return (
       <div className='App'>
+        <input
+          type='search'
+          placeholder='Search For a Monster'
+          onChange={e => this.setState({ searchField: e.target.value })}
+        />
         <CardList monsters={this.state.monsters} />
-      
       </div>
     );
   }
