034cb8a6c88a1d15f55be470d6f27f50280e3536
Eventyret <simen.dehlin@gmail.com>
2019-09-05 17:56:38 +0100

    Implemented filter for monsters

--- src/App.js
+++ src/App.js
@@ -19,6 +19,8 @@ class App extends Component {
   }
 
   render() {
+    const { monsters, searchField } = this.state;
+    const filteredMonsters = monsters.filter(monster => monster.name.toLowerCase().includes(searchField.toLowerCase()));
     return (
       <div className='App'>
         <input
@@ -26,7 +28,7 @@ class App extends Component {
           placeholder='Search For a Monster'
           onChange={e => this.setState({ searchField: e.target.value })}
         />
-        <CardList monsters={this.state.monsters} />
+        <CardList monsters={filteredMonsters} />
       </div>
     );
   }
