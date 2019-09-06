1b8026d0702b5181bac88e566bcc360c7d4ba7ce
Eventyret <simen.dehlin@gmail.com>
2019-09-06 11:48:26 +0100

    Updates

---
+++ precache-manifest.4d1f48ade2ec6716ce1233f336c5603d.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "638c01ca33b068399f1d18dc9d8af5a4",
+    "url": "/MonsterDex/index.html"
+  },
+  {
+    "revision": "59eb2f5bf63f1aa7f5b5",
+    "url": "/MonsterDex/static/css/main.a15d24c6.chunk.css"
+  },
+  {
+    "revision": "472ab3c54422111b6e77",
+    "url": "/MonsterDex/static/js/2.05fc6efa.chunk.js"
+  },
+  {
+    "revision": "59eb2f5bf63f1aa7f5b5",
+    "url": "/MonsterDex/static/js/main.f72083a4.chunk.js"
+  },
+  {
+    "revision": "569e9557dd169f6d7c89",
+    "url": "/MonsterDex/static/js/runtime~main.4cd9dde6.js"
+  }
+]);
