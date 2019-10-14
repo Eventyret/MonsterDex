3a357f959f2a8dc90ed56a31fcebd08ea5f3fdb7
Simen Daehlin <DaehlinS@FORTIS-UK.FORTIS.COM>
2019-10-14 15:13:05 +0100

    Updated with more scripts

--- testing/script.test.js
+++ testing/script.test.js
@@ -10,3 +10,12 @@ it('is searching google', () => {
   expect(googleSeach('testtest', dbMock)).toEqual([]);
   expect(googleSeach('dog', dbMock)).toEqual(['dog.com', 'dogpictures.com']);
 });
+
+it('work with undefined and null input', () => {
+  expect(googleSeach(undefined, dbMock)).toEqual([]);
+  expect(googleSeach(null, dbMock)).toEqual([]);
+});
+
+it('does not return more then 3 matches', () => {
+  expect(googleSeach('.com', dbMock).length).toEqual(3);
+});
