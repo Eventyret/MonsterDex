735932d1a5e7a9d2bfefef1187d967f3d01ffb85
Simen Daehlin <DaehlinS@FORTIS-UK.FORTIS.COM>
2019-10-14 14:28:29 +0100

    Added a simple test

--- testing/script.js
+++ testing/script.js
@@ -8,11 +8,13 @@ const googleDatabase = [
   'myfavouritecats2.com'
 ];
 
-const googleSearch = searchInput => {
-  const matches = googleDatabase.filter(website => {
+const googleSearch = (searchInput, db) => {
+  const matches = db.filter(website => {
     return website.includes(searchInput);
   });
   return matches.length > 3 ? matches.slice(0, 3) : matches;
 };
 
-console.log(googleSearch('cat'));
+console.log(googleSearch('cat', googleDatabase));
+
+module.exports = googleSearch;
--- testing/script.test.js
+++ testing/script.test.js
@@ -1,3 +1,12 @@
-it('this is a test', () => {
-	
-})
+const googleSeach = require('./script');
+
+const dbMock = ['dog.com', 'cheesepuff.com', 'disney.com', 'dogpictures.com'];
+it('this is a silly test', () => {
+  expect('hello').toBe('hello');
+  googleSeach('testtest', dbMock);
+});
+
+it('is searching google', () => {
+  expect(googleSeach('testtest', dbMock)).toEqual([]);
+  expect(googleSeach('dog', dbMock)).toEqual(['dog.com', 'dogpictures.com']);
+});
