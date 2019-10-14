8e854651a1c6bf51df37e76dcd505ee10f7b1593
Simen Daehlin <DaehlinS@FORTIS-UK.FORTIS.COM>
2019-10-14 15:14:24 +0100

    Grouped with describe

--- testing/script.test.js
+++ testing/script.test.js
@@ -1,21 +1,24 @@
 const googleSeach = require('./script');
 
 const dbMock = ['dog.com', 'cheesepuff.com', 'disney.com', 'dogpictures.com'];
-it('this is a silly test', () => {
-  expect('hello').toBe('hello');
-  googleSeach('testtest', dbMock);
-});
 
-it('is searching google', () => {
-  expect(googleSeach('testtest', dbMock)).toEqual([]);
-  expect(googleSeach('dog', dbMock)).toEqual(['dog.com', 'dogpictures.com']);
-});
+describe('googleSearch', () => {
+  it('this is a silly test', () => {
+    expect('hello').toBe('hello');
+    googleSeach('testtest', dbMock);
+  });
 
-it('work with undefined and null input', () => {
-  expect(googleSeach(undefined, dbMock)).toEqual([]);
-  expect(googleSeach(null, dbMock)).toEqual([]);
-});
+  it('is searching google', () => {
+    expect(googleSeach('testtest', dbMock)).toEqual([]);
+    expect(googleSeach('dog', dbMock)).toEqual(['dog.com', 'dogpictures.com']);
+  });
 
-it('does not return more then 3 matches', () => {
-  expect(googleSeach('.com', dbMock).length).toEqual(3);
+  it('work with undefined and null input', () => {
+    expect(googleSeach(undefined, dbMock)).toEqual([]);
+    expect(googleSeach(null, dbMock)).toEqual([]);
+  });
+
+  it('does not return more then 3 matches', () => {
+    expect(googleSeach('.com', dbMock).length).toEqual(3);
+  });
 });
