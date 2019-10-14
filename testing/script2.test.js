b44a6c689be9f0350af62d8a87212fc62f9cfa5f
Simen Daehlin <DaehlinS@FORTIS-UK.FORTIS.COM>
2019-10-14 15:39:47 +0100

    Used done to test async

---
+++ testing/script2.test.js
@@ -0,0 +1,15 @@
+const fetch = require('node-fetch');
+const swapi = require('./script2');
+
+it('calls swapi to get people', done => {
+  expect.assertions(1);
+  swapi.getPeople(fetch).then(data => {
+    expect(data.count).toEqual(87);
+    done();
+  });
+});
+it('calls swapi to get people with a promise', () => {
+  swapi.getPeople(fetch).then(data => {
+    expect(data.count).toEqual(87);
+  });
+});
