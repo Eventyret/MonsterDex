f1f8ed8f40e4ff830ebd683c296816d6a4496806
Simen Daehlin <DaehlinS@FORTIS-UK.FORTIS.COM>
2019-10-14 15:45:02 +0100

    Added to use promise

--- testing/script2.test.js
+++ testing/script2.test.js
@@ -1,15 +1,16 @@
 const fetch = require('node-fetch');
 const swapi = require('./script2');
 
-it('calls swapi to get people', done => {
+it('calls swapi to get people', () => {
   expect.assertions(1);
-  swapi.getPeople(fetch).then(data => {
+  return swapi.getPeople(fetch).then(data => {
     expect(data.count).toEqual(87);
-    done();
   });
 });
 it('calls swapi to get people with a promise', () => {
-  swapi.getPeople(fetch).then(data => {
+  expect.assertions(2);
+  return swapi.getPeople(fetch).then(data => {
     expect(data.count).toEqual(87);
+    expect(data.results.length).toBeGreaterThan(5);
   });
 });
